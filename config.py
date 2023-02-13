63f6411c09cebc196ffa7be61ecc18fb3151ca23
timofeyvoltov <voltov-tom@mail.ru>
2023-02-13 20:28:30 +0400

    added dotenv environment

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.getenv('PEPWM_BOT_API_ID')
+API_HASH = os.getenv('PEPWM_BOT_API_HASH')
+BOT_TOKEN = os.getenv('PEPWM_BOT_BOT_TOKEN')
