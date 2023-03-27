70dd673e9ae63e2fbd040e1f74a3c81cd2046400
timofeyvoltov <voltov-tom@mail.ru>
2023-03-27 20:09:46 +0400

    fixed

--- main.py
+++ main.py
@@ -15,68 +15,6 @@ bot = telebot.TeleBot(TG_BOT_TOKEN)
 bot_name = bot.get_me().username
 
 
-@bot.message_handler(commands=['тест'])
-def tag_all_participant_test(message):
-    chat_id = message.chat.id
-    print('тест', ctime(time()))
-    print('chat_id:', str(chat_id))
-    if chat_id != -1001787523639:  # КЛПД
-        return
-
-    print(get_random_picture_src())
-    print(get_random_gif_src())
-
-    from_user = message.from_user.username
-    all_users = get_all_chat_users(chat_id)
-    all_users_count = len(all_users)
-    group_users = 0
-    count = 0
-    participants = ''
-
-    for user in all_users:
-        print(count)
-        print(user.username) if user.username else print(user.first_name)
-
-        # пропускаем, если отправитель или бот
-        if user.username == from_user or user.bot:
-            count += 1
-            print(from_user)
-            continue
-
-        # eсли нет username, тегаем по first_name
-        if user.username is None and user.first_name:
-            mention = f'Message: <a href="tg://user?id={user.id}">{user.first_name}</a>'
-            count += 1
-            print(mention)
-            continue
-
-        participant = '@' + user.username
-
-        # специально для mrRozhin
-        if participant == '@mrRozhin':
-            print(f'Message: {participant}: "Сосать господин судья"')
-        # специально для стаса и сережи
-        elif from_user == 'stasucan' and participant == '@gnu_brsk':
-            print(f'Message: Эй пидр! {participant} ')
-        elif from_user == 'gnu_brsk' and participant == '@stasucan':
-            print(f'Message: Эй пидр! {participant} ')
-        else:
-            participants += participant + ' '
-            group_users += 1
-            print(participants)
-        count += 1
-
-        # отправляем пачкой по 5 или то, что осталось
-        if group_users % 5 == 0 or count == all_users_count:
-            # message_part = random.choice(WAR_CRY)
-            # bot.send_message(
-            #     message.chat.id, f'{message_part} {participants} '
-            # )
-            print(f'Message: {participants}')
-            participants = ''
-            sleep(0.5)
-
-
 @bot.message_handler(commands=['ъ'])
 def tag_all_participant(message):
     """
@@ -89,8 +27,10 @@ def tag_all_participant(message):
         return
 
     try:
-        bot.send_video(message.chat.id, get_random_gif_src())
-    except Exception:
+        gif = open(get_random_gif_src(), 'rb')
+        bot.send_animation(message.chat.id, gif)
+        gif.close()
+    except:
         bot.send_photo(message.chat.id, 'https://memepedia.ru/wp-content/uploads/2017/07/%D1%85%D0%BE%D1%85%D0%BE%D1%87%D1%83%D1%89%D0%B8%D0%B9-%D0%B8%D1%81%D0%BF%D0%B0%D0%BD%D0%B5%D1%86.jpg')
 
     from_user = message.from_user.username
--- parsing.py
+++ parsing.py
@@ -1,5 +1,8 @@
+from random import choice
+
 import requests
 
+from os import walk
 from bs4 import BeautifulSoup
 
 
@@ -13,13 +16,31 @@ def get_random_picture_src():
 
 
 def get_random_gif_src():
-    url = 'https://joyreactor.cc/tag/гифки'
-    site = requests.get(url)
-    soup = BeautifulSoup(site.text, 'lxml')
+    site = 'https://joyreactor.cc/tag/гифки'
+    response = requests.get(site)
+    soup = BeautifulSoup(response.text, 'lxml')
     span = soup.find('a', {'class': 'video_gif_source'})
-    return 'https:' + span['href']
+
+    gif_name = span['href'][-11:]
+    gif_local_name = './gifs/' + gif_name
+
+    with open(gif_local_name, 'wb') as f:
+        response = requests.get('https:' + span['href'])
+        f.write(response.content)
+
+    return gif_local_name
+
+
+def get_random_gif_src_local():
+    path = '/Users/timofeyvoltov/Desktop/gifs/'
+    gifs = []
+
+    for (dirpath, dirnames, filenames) in walk(path):
+        gifs.extend(filenames)
+        break
+
+    return path + choice(gifs)
 
 
 if __name__ == '__main__':
-    print('https:' + get_random_picture_src())
-    print('https:' + get_random_gif_src())
+    print(get_random_gif_src())
